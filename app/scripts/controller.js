9b22c58d26ba3bd33e5a4cb8a59f6f00a3d2546f
seanmyers <sean.myers@nbtsolutions.com>
2015-12-31 14:11:11 -0500

    used the $resource method update to push new content to JSON

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -6,11 +6,19 @@ angular.module('confusionApp')
 
             $scope.tab = 1;
             $scope.filtText = '';
-            $scope.showDetails = true;
-            $scope.showMenu = true;
+            //$scope.showDetails = false;
+            $scope.showMenu = false;
             $scope.message = "Loading ...";
 
-            $scope.dishes = menuFactory.getDishes().query();
+            $scope.dishes = menuFactory.getDishes().query(
+                function(response) {
+                    $scope.dishes = response;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
 
 
             $scope.select = function(setTab) {
@@ -72,27 +80,40 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message = "Loading ...";
 
-            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+                .$promise.then (
+                    function(response) {
+                        $scope.dish = response;
+                        $scope.showDish = true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                    }
+                );
 
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope,menuFactory) {
 
-            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            $scope.myreview = {rating:5, comment:"", author:"", date:""};
 
             $scope.submitComment = function () {
 
-                $scope.mycomment.date = new Date().toISOString();
-                console.log($scope.mycomment);
+                $scope.myreview.date = new Date().toISOString();
+                console.log('test');
+                console.log($scope.myreview);
 
-                $scope.dish.comments.push($scope.mycomment);
+
+                $scope.dish.comments.push($scope.myreview);
+
+                // add the new comment to the JSON on the server
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
 
                 $scope.commentForm.$setPristine();
-
-                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+                $scope.myreview = {rating:5, comment:"", author:"", date:""};
             };
         }])
 
@@ -100,10 +121,19 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', '$stateParams', 'menuFactory',
             'corporateFactory', function($scope,$stateParams,menuFactory,corporateFactory) {
 
-                $scope.showDish = true;
+                $scope.showDish = false;
                 $scope.message="Loading ...";
 
-                $scope.featuredish = menuFactory.getDishes().get({id:0});
+                $scope.featuredish = menuFactory.getDishes().get({id:0})
+                    .$promise.then (
+                        function(response) {
+                            $scope.featuredish = response;
+                            $scope.showDish = true;
+                        },
+                        function(response) {
+                            $scope.message = "Error: "+response.status + " " + response.statusText;
+                        }
+                    );
 
                 $scope.promotion = menuFactory.getPromotion(0);
                 $scope.leader = corporateFactory.getLeader(3);
